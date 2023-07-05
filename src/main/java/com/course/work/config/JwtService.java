6014730fb64a74659ed24a9beb6f6c38bacbc86f
NfolZordo <Nfolzordo@gmail.com>
2023-07-05 14:10:51 +0300

    docker add

--- src/main/java/com/course/work/config/JwtService.java
+++ src/main/java/com/course/work/config/JwtService.java
@@ -40,8 +40,8 @@ public class JwtService {
         .setClaims(extraClaims)
         .setSubject(userDetails.getUsername())
         .setIssuedAt(new Date(System.currentTimeMillis()))
-//            .setExpiration(new Date(System.currentTimeMillis() +  7))
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+            // .setExpiration(new Date(System.currentTimeMillis() +  7))
+           .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
         .signWith(getSignInKey(), SignatureAlgorithm.HS256)
         .compact();
   }
